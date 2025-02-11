4b8d91aac9160e67b1f2946c722ac3c71aede7f5
Oleksandr Borovyk <sasha.borovyk03@gmail.com>
2025-02-11 04:03:32 +0200

    03 - added registration of Users (without encrypting passwords and simply adding users to database)

---
+++ src/main/java/com/sasha/jobportal/services/UsersTypeService.java
@@ -0,0 +1,24 @@
+package com.sasha.jobportal.services;
+
+import com.sasha.jobportal.entity.UsersType;
+import com.sasha.jobportal.repository.UsersTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsersTypeService {
+
+    private final UsersTypeRepository usersTypeRepository;
+
+    @Autowired
+    public UsersTypeService(UsersTypeRepository usersTypeRepository) {
+        this.usersTypeRepository = usersTypeRepository;
+    }
+
+    public List<UsersType> getAll() {
+        return usersTypeRepository.findAll();
+    }
+
+}
