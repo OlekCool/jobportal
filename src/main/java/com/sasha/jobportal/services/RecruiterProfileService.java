c473ed0a6db1d5448abf29ed78ff8e4898d4a967
Oleksandr Borovyk <sasha.borovyk03@gmail.com>
2025-02-13 15:59:34 +0200

    05 - did creation of Recruiter profile with all needed data and profile photo

---
+++ src/main/java/com/sasha/jobportal/services/RecruiterProfileService.java
@@ -0,0 +1,27 @@
+package com.sasha.jobportal.services;
+
+import com.sasha.jobportal.entity.RecruiterProfile;
+import com.sasha.jobportal.repository.RecruiterProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RecruiterProfileService {
+
+    private final RecruiterProfileRepository recruiterRepository;
+
+    @Autowired
+    public RecruiterProfileService(RecruiterProfileRepository recruiterRepository) {
+        this.recruiterRepository = recruiterRepository;
+    }
+
+    public Optional<RecruiterProfile> getOne(Integer id) {
+        return recruiterRepository.findById(id);
+    }
+
+    public RecruiterProfile addNew(RecruiterProfile recruiterProfile) {
+        return recruiterRepository.save(recruiterProfile);
+    }
+}
